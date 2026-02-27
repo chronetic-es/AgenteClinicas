4f1d41c572c4a2e844cf22a9a34f90ed709851a9
chronetic-es <A22AdrianRV@iessanclemente.net>
2026-02-27 18:53:44 +0100

    Changed config

--- mcp_server/calendario.py
+++ mcp_server/calendario.py
@@ -1,16 +1,17 @@
 import datetime
 import os.path
+import config
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
-SERVICE_ACCOUNT_FILE = 'credentials.json'
+
 
 def getCalendarInstance():
 
-    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE,scopes=SCOPES)
+    credentials = service_account.Credentials.from_service_account_file(config.SERVICE_ACCOUNT_FILE,scopes=SCOPES)
 
     try:
         service = build("calendar","v3",credentials=credentials)
