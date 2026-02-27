37e77c27742809e7df23926b431446df55a6e0a8
chronetic-es <A22AdrianRV@iessanclemente.net>
2026-02-27 13:11:54 +0100

    Changed calendario.py

--- mcp_server/calendario.py
+++ mcp_server/calendario.py
@@ -22,7 +22,7 @@ def getCalendarInstance():
             flow = InstalledAppFlow.from_client_secrets_file(
             "credentials.json", SCOPES
             )
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
             with open("token.json","w") as token:
                 token.write(creds.to_json())
     try:
