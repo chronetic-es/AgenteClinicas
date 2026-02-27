02be690f4459d06a47a605b61078e51d2aa6df72
chronetic-es <A22AdrianRV@iessanclemente.net>
2026-02-27 20:36:49 +0100

    Removed json load

--- mcp_server/calendario.py
+++ mcp_server/calendario.py
@@ -1,7 +1,6 @@
 import datetime
 import os.path
 import config
-import json
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -13,7 +12,7 @@ SERVICE_ACCOUNT_INFO = config.CREDENTIALS
 
 def getCalendarInstance():
 
-    credentials = service_account.Credentials.from_service_account_info(json.load(SERVICE_ACCOUNT_INFO),scopes=SCOPES)
+    credentials = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT_INFO,scopes=SCOPES)
 
     try:
         service = build("calendar","v3",credentials=credentials)
